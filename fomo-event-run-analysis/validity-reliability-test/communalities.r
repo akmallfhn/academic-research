6f3fe35de98b886df36ab3769ac2543f667f435f
Akmal Luthfiansyah <akmalluthfiansyah@gmail.com>
2025-11-30 22:16:44 +0700

    Add Communalities

--- fomo-event-run-analysis/validity-reliability-test/communalities.r
+++ fomo-event-run-analysis/validity-reliability-test/communalities.r
@@ -1,1 +1,22 @@
+library(psych)
+
 df <- read.csv("questioner.csv", header = TRUE, sep = ",")
+
+# Pilih hanya numeric columns
+items <- df[sapply(df, is.numeric)]
+
+# Hitung communalities (Initial & Extraction)
+# Initial = nilai komunialitas awal dari Principal Component Analysis (PCA)
+# Extraction = komunlitas setelah faktor diekstraksi.
+pca_res <- principal(items, nfactors = 1, rotate = "none")
+
+comm <- pca_res$communality
+
+communalities <- data.frame(
+    Item = names(comm),
+    Initial = rep(1, length(comm)),
+    Extraction = as.numeric(comm)
+)
+
+print("=== COMMUNALITIES ===")
+print(communalities)
